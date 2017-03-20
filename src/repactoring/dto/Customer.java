c245798096e154418076c45067d0c707d80fb6b4
LJW <LJW@DESKTOP-A3G7RKS>
2017-03-20 12:20:15 +0900

    메서드 추출기법 완료

--- src/repactoring/dto/Customer.java
+++ src/repactoring/dto/Customer.java
@@ -21,7 +21,7 @@ public class Customer {
     	   this.rentals.add(aRental);
        }
        
-       public String htmlstatement(){
+       public String statement(){
     	   //대여료와 적립포인트 출력
     	   double totalAmount = 0;//대여료
     	   int frequentRenerPoints =0;//적립포인트
@@ -29,36 +29,12 @@ public class Customer {
     	   sb.append("<h1><Em>"+getName() + "고객님의 대여기록</Em></h1><br>\n");
     	   
     	   for(Rental each : rentals){
-    		   double thisAmount = 0;//비디오물당 대여료
+    		   double thisAmount = amountFor(each);//비디오물당 대여료
   			//1. 일반물(2일) 1500원, 일일 초과 1500, 적립포인트1 ==> (2일, 3000원 )일일 초과 2000, 적립0.5
  			//2. 아동물(3일) 1500원, 일일 초과 1500, 적립1 ==>(2일, 2000원) 일일 초과1500, 적립1
    			//3. 최신물(1일) 3000원, 일일 초과 3000, 적립 1+1 ==>(2일, 4000원) 일일초과 4000. 적립 1+1
     		   //html형식을 출력 원함.
-    		   Movie movie = each.getMovie();
-    		   int priceCode = movie.getPriceCode();
-    		   int daysRented = each.getDaysRented();//해당 비디오물 대여시간
-    		   
-    		   switch(priceCode){
-    		   case Movie.REGULAR:
-    			   thisAmount = 3000;
-    			   if (daysRented >2){
-    				   thisAmount += (daysRented-2) * 2000;
-    			   }
-    		   break;
-    		   case Movie.NEW_RELEASE:
-    			   thisAmount = 4000;
-    			   if (daysRented >3){
-    				   thisAmount = daysRented + 3000;
-    			   }
-    			   
-    		   break;
-    		   case Movie.CHILDREN:
-    			   thisAmount = 300;
-    			   if(daysRented >3){
-    				   thisAmount += (daysRented-3)* 1500;
-    			   }
-    		   break;
-    		   }
+    		  
     	   
     	    frequentRenerPoints++;
     	    if (each.getMovie().getPriceCode()==Movie.NEW_RELEASE&& each.getDaysRented()>1){
@@ -75,4 +51,34 @@ public class Customer {
     	   return sb.toString();
     	   
        }
+
+	private double amountFor(Rental aRental) {
+		double result = 0;
+		Movie movie = aRental.getMovie();
+		   int priceCode = movie.getPriceCode();
+		   int daysRented = aRental.getDaysRented();//해당 비디오물 대여시간
+		   
+		   switch(priceCode){
+		   case Movie.REGULAR:
+			   result = 3000;
+			   if (daysRented >2){
+				   result += (daysRented-2) * 2000;
+			   }
+		   break;
+		   case Movie.NEW_RELEASE:
+			   result = 4000;
+			   if (daysRented >3){
+				   result = daysRented + 3000;
+			   }
+			   
+		   break;
+		   case Movie.CHILDREN:
+			   result = 300;
+			   if(daysRented >3){
+				   result += (daysRented-3)* 1500;
+			   }
+		   break;
+		   }
+		return 0;
+	}
 }
