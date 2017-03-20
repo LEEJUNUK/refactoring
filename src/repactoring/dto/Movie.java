efffdb7554a3dc9e6e1252b37f4958a20b2863f6
LJW <LJW@DESKTOP-A3G7RKS>
2017-03-20 10:23:01 +0900

    movie class 정의 완료

---
+++ src/repactoring/dto/Movie.java
@@ -0,0 +1,28 @@
+package repactoring.dto;
+
+public class Movie {
+      private String title;
+      private int priceCode;
+
+      
+      
+      public Movie(String title, int priceCode) {
+		super();
+		this.title = title;
+		this.priceCode = priceCode;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPriceCode() {
+		return priceCode;
+	}
+	public void setPriceCode(int priceCode) {
+		this.priceCode = priceCode;
+	}
+      
+      
+}
